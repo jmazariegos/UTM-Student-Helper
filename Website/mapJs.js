1f1db0aedff15c609ab43b50acaf906608c39611
Anthony Gruskovnjak <AGruskovnjak@Anthonys-MacBook-Air.local>
2019-03-21 20:39:59 -0400

    push

--- Website/mapJs.js
+++ Website/mapJs.js
@@ -3,24 +3,51 @@
 
 function loadBuilding(building) {
     // Building is the building name
+    var url = "./Website/TempFolder/Buildings.json";
+    console.log("Loaded Building "+ building);
     $.getJSON(url, function(data) {
             console.log("In json request");
             console.log(data);
+            var table = document.getElementById("mapButtons");
+            let placeJSON = null;
+
             var build = data["buildings"];
             for (var i=0; i < build.length; i++){
-                var row = table.insertRow(0);
-                var cell1 = row.insertCell(0);
-                console.log(build[i].code);
-                cell1.innerHTML = build[i].code;
+                if (build[i].code == building){
+                    placeJSON=build[i]
+                    break;
+                }
             }
+
+            if(placeJSON == null){
+                console.log("Shouldn't be here");
+                return;
+            }
+
+
+            // Take apart button table
+            while (table.rows.length>0){
+                table.deleteRow(0);
+            }
+            // Load Current Object
+
+            //Change Image + Titlie
+            var title = document.getElementById("mapTitle");
+            title.innerHTML = placeJSON.name;
+
+
+            var pic = document.getElementById("mapImg");
+            pic.src ="images/parking-map.PNG"; // This to be changed to proper image
+
+            // Add New Buttons for each floor
+
+            // Add Apropriate listeners
+            
         }
     ).fail(function( jqxhr, textStatus, error ) {
     var err = textStatus + ", " + error;
-    console.log( "Request Failed: " + err );
+    console.log("Request Failed: " + err);
     });
-    
-
-
     return;
 }
 
@@ -28,6 +55,10 @@ function loadMain(){
     // Create the title
     var title = document.getElementById("mapTitle");
     title.innerHTML = "MAP OF UTM";
+
+    var pic = document.getElementById("mapImg");
+    pic.src ="images/campus-map.jpg";
+
     // Loads the main Map -> Special ids for button names
     var table = document.getElementById("mapButtons");
 
@@ -51,15 +82,15 @@ function loadMain(){
                     return function() {
                         var cell = row.getElementsByTagName("td")[0];
                         var id = cell.innerHTML;
-                        alert("id:" + id);
+                        console.log("id:" + id);
+                        loadBuilding(id);
                     };
                   };
-    row.onclick = createClickHandler(row);
-                //cell.onclick = function() {console.log(cell.innerHTML +" was pressed") };
+                row.onclick = createClickHandler(row);
                 cell.innerHTML = build[i].code;
             }
         }
-    ).fail(function( jqxhr, textStatus, error ) {
+    ).fail(function( jqxhr, textStatus, error){
     var err = textStatus + ", " + error;
     console.log( "Request Failed: " + err );
 });
